f4db4482d26a7518da46b6c1f848272899d6ec8f
André <fxandrealb@gmail.com>
2024-12-08 18:55:22 +0000

    issue with insecure connection

--- srcs/requirements/nginx/tools/certs.sh
+++ srcs/requirements/nginx/tools/certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/my_cert/$ssl_certificate/g" /etc/nginx/sites-available/default
-sed -i "s/my_key/$ssl_certificate_key/g" /etc/nginx/sites-available/default
-sed -i "s/DOMAIN_NAME/$nginx_domain/g" /etc/nginx/sites-available/default
+sed -i "s|my_cert|${SSL_CERT_PATH}|g" /etc/nginx/sites-available/default
+sed -i "s|my_key|${SSL_KEY_PATH}|g" /etc/nginx/sites-available/default
+sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|g" /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
