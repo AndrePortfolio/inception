5c051a48d9dc67d8ab5f1ed9ac22f433dc201032
André <fxandrealb@gmail.com>
2024-12-09 13:46:52 +0000

    mostly works

--- srcs/requirements/nginx/tools/certs.sh
+++ srcs/requirements/nginx/tools/certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s|my_cert|${SSL_CERT_PATH}|g" /etc/nginx/sites-available/default
-sed -i "s|my_key|${SSL_KEY_PATH}|g" /etc/nginx/sites-available/default
 sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|g" /etc/nginx/sites-available/default
+sed -i "s|my_key|${SSL_KEY_PATH}|g" /etc/nginx/sites-available/default
+sed -i "s|my_cert|${SSL_CERT_PATH}|g" /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
